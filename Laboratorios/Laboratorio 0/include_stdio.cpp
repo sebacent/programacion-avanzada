8e0a22c75694186db3bd626cda87f83413fb6d98
“[sebastian <[sebastiancenturion27@gmail.com]>
2023-03-31 16:22:29 -0300

    Agrego lab0.h, modificamos .cpp

--- Laboratorios/Laboratorio 0/include_stdio.cpp
+++ Laboratorios/Laboratorio 0/include_stdio.cpp
@@ -1,81 +1,41 @@
-#include <stdio.h>
+#include<lab0.h>
+#include<stdio.h>
+
 using namespace std;
 
-const MAX_EMPLEADOS =50;
+float cambio::a_peso(float paga){
+    Moneda cambio = 0;
+    return paga * cambio;
+}
+float cambio::a_dolar(float paga){
+    Moneda cambio = 1;
+    if (paga != 0){
+        return paga / cambio;
+    } else {
+        return paga;
+    }
+    
+}
+
+Paga::paga(){
+    this->monto = 0;
+    this->moneda = 1;
+
+}
 
 
-class empleado (
-private:
-    string nombre;
-    string ci;
-    int edad;
-    paga valor_paga;
-public:
-    float  get_sueldo_peso();
-    float get_sueldo_dolar();
+paga Paga::a_dolar(float sueldoP){
+    return cambio::a_dolar(sueldoP);
+}
+
+paga Paga::a_peso(float sueldoUS){
+    return cambio::a_peso(sueldoUS);
+}
 
 
-
-);
-
-
-class fijo : public empleado(
-
-public:
-    float get_sueldo_peso();
-    float get_sueldo_dolar();
-
-);
-
-
-
-class jornalero: public empleado(
-private:
-     int horas;
-
-public:
-    float get_sueldo_peso();
-    float get_sueldo_dolar();
-
-)
-
-
-class empresa (
-
-private:
-    string nombre;
-    string nombre_legal;
-    int rut;
-public:
-    paga  total_sueldo_peso();
-    paga  total_sueldo_dolar();
-
-);
-
-class paga(
-
-public:
-    float monto;
-    Moneda moneda;
-private:
-     paga a_dolar();
-     paga a_peso();
-
-
-)
-
-class cambio(
-public:
- float a_peso(float);
- float a_dolar(float);
-);
-
-
-
-
-
-
-enum Moneda{
-usd =40.15,
-us =1
-};
+empleado::empleado(string nombre, string ci, int edad, paga valor_hora){
+    this->nombre = nombre;
+    this->ci = ci;
+    this->edad = edad;
+    this->valor_hora = valor_hora;
+}
